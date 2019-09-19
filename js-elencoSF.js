7d9ce3e092fcc1873f39d80ed6394a1d1b49db43
evertonschultz <evertonf.m.schultz98@gmail.com>
2019-09-19 11:33:36 -0300

    primeiro commit

---
+++ js-elencoSF.js
@@ -0,0 +1,31 @@
+function mudaImagem(i) {  
+	
+	if (i == 0) {
+        document.getElementById("ideia").src="elenco/RenatoPortaluppi.jpg"
+    } if (i == 1) {
+        document.getElementById("ideia").src="elenco/Luan.jpg";    
+    } if (i == 2) {       
+        document.getElementById("ideia").src="elenco/PedroRocha.jpg";    
+    } if (i == 3) {
+        document.getElementById("ideia").src="elenco/Arthur.jpg";    
+    } if (i == 4) {
+        document.getElementById("ideia").src="elenco/Ramiro.jpg";    
+    } if (i == 5) {
+        document.getElementById("ideia").src="elenco/Maicon.jpg";    
+    } if (i == 6) {
+        document.getElementById("ideia").src="elenco/Michel.jpg";    
+    } if (i == 7) {
+        document.getElementById("ideia").src="elenco/BrunoCortez.jpg";    
+    } if (i == 8) {
+        document.getElementById("ideia").src="elenco/Kannemann.jpg";    
+    } if (i == 9) {
+        document.getElementById("ideia").src="elenco/Geromel.jpg";    
+    } if (i == 10) {
+        document.getElementById("ideia").src="elenco/Edilson.jpg";    
+    } if (i == 11) {
+        document.getElementById("ideia").src="elenco/MarceloGrohe.jpg";    
+    }
+}
+function bug() {
+    alert("Desculpe! Está Página Está Em Manutenção!!");
+}
